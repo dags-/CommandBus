70eae4cf8608de4563059f01042edf2748ad7a88
dags <dags-@users.noreply.github.com>
2016-03-10 12:05:47 +0000

    Update One.java

--- src/main/java/me/dags/commandbus/annotation/One.java
+++ src/main/java/me/dags/commandbus/annotation/One.java
@@ -40,5 +40,10 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface One
 {
+    /**
+     * Optionally set the name of this variable (used in generated command info).
+     *
+     * @return The name of the Parameter this annotation is annotating.
+     */
     String value() default "";
 }
